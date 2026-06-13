f5b5693ef3b7020bd6106ddf38628a1ec56477ee
JoseRamirez <jr199705@gmial.com>
2026-06-13 09:45:21 -0600

    created verification and tested

--- main.py
+++ main.py
@@ -16,18 +16,25 @@ df.info()
 
 #Data Cleaning and Standarization
 
-df['migration_date_clean'] = pd.to_datetime(df['migration_dates'], errors='coerce')
+df['migration_date_clean'] = pd.to_datetime(df['migration_date'], errors='coerce')
 
-original_not_null = df['migration_dates'].notna()
-failed_rows = df[original_not_null] & df['migration_dates'].isna()
+original_not_null = df['migration_date'].notna()
+failed_rows = df[original_not_null & df['migration_date_clean'].isna()]
 
 if not failed_rows.empty:
-    print(f"⚠️ Alerta de Migración: Se encontraron {len(failed_rows)} registros con formatos inválidos.")
+    print(f" Alerta de Migración: Se encontraron {len(failed_rows)} registros con formatos inválidos.")
     df_failed_dates = pd.DataFrame(failed_rows)
     df_failed_dates.to_csv('failed_date.csv')
     print("File 'failed_date.csv' successfully generated!")
 
-df['full_name'] = df['full_name'].fillna)("MISSING NAME")
+df['full_name'] = df['full_name'].fillna("MISSING NAME")
 
 df['full_name'] = df['full_name'].str.strip().str.title()
 
+# verification
+
+print("\nCleaned Data Preview")
+print(df.head(10))
+
+print("\nFinal Inspection")
+df.info()
